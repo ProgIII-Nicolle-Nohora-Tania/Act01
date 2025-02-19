b194dac94a5d0e36036327c1374f009cb4153d29
Nohora Cardozo <nohora.cardozo@utp.edu.co>
2025-02-18 22:10:39 -0500

    Implementaación de la regla que permite inferir si X es criminal.

--- coronelWest.pl
+++ coronelWest.pl
@@ -1,3 +1,6 @@
 enemigo_de(coreaDelSur, estadosUnidos).
 estadounidense(cWest).
 vendio_misiles(cWest, coreaDelSur).
+
+es_criminal(X) :- estadounidense(X), vendio_misiles(X, Y), enemigo_de(Y, 
+estadosUnidos).
